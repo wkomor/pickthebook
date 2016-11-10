4ae64bce058507d2e2fedc26c8e5a982d5969ac9
Vitold Komorovski <komorowskiws@gmail.com>
2016-11-10 20:31:34 +0700

    gulp added

---
+++ Gulpfile.js
@@ -0,0 +1,61 @@
+/**
+ * Created by vitold on 07.11.16.
+ */
+'use strict';
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    prefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rigger = require('gulp-rigger'),
+    cssmin = require('gulp-clean-css'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    rimraf = require('rimraf'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload;
+
+var path = {
+    build: { //Тут мы укажем куда складывать готовые после сборки файлы
+        html: 'build/',
+        js: 'build/js/',
+        css: 'build/css/',
+        img: 'build/img/',
+        fonts: 'build/fonts/'
+    },
+    src: { //Пути откуда брать исходники
+        html: 'templates/*.html', //Синтаксис templates/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+        js: 'assets/js/main.js',//В стилях и скриптах нам понадобятся только main файлы
+        style: 'assets/css/main.css',
+        img: 'assets/img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+        fonts: 'assets/fonts/**/*.*'
+    },
+    watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+        html: 'templates/**/*.html',
+        js: 'assets/js/**/*.js',
+        style: 'assets/css/**/*.scss',
+        img: 'assets/img/**/*.*',
+        fonts: 'assets/fonts/**/*.*'
+    },
+    clean: './build'
+};
+
+var config = {
+    server: {
+        baseDir: "./build"
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 8000,
+    logPrefix: "Frontend_Devil"
+};
+
+gulp.task('html:build', function () {
+    gulp.src(path.src.html) //Выберем файлы по нужному пути
+        .pipe(rigger()) //Прогоним через rigger
+        .pipe(gulp.dest(path.build.html)) //Выплюнем их в папку build
+        .pipe(reload({stream: true})); //И перезагрузим наш сервер для обновлений
+});
+
